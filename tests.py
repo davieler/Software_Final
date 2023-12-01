5c36c5f5a8167e013dfaa53f29c068ec68a0660d
davieler <83974308+davieler@users.noreply.github.com>
2023-11-30 21:00:55 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,6 +6,37 @@ from main import app
 client = TestClient(app)
 class TestBilleteraEndpoints(unittest.TestCase):
 
+    def test_read_contactos(self):
+        # Success case
+        response = client.get("/billetera/contactos/minumero=123")
+        assert response.status_code == 200
+        #mi response sale exactamente {"contactos": [["Andrea", "456"]]} pero tengo error de assert
+        assert response.json() == {"contactos": [["Andrea", "456"]]}
+        
+        # Multiple contacts
+        
+        response = client.get("/billetera/contactos/minumero=21345")
+        assert response.status_code == 200
+        #mi response cuando hago /billetera/contactos/minumero=21345 sale bien pero el assert dice error
+        assert response.json() == {"contactos": [["Luisa", "123"], ["Andrea", "456"]]}
+
+
+        # Error cases
+        # Case 1: Non-existent number
+        response = client.get("/billetera/contactos/minumero=999")
+        assert response.status_code == 404
+        assert response.json() == {"detail": "El numero de telefono no existe"}
+
+        # Case 2: Invalid input type
+        response = client.get("/billetera/contactos/minumero=abc")
+        assert response.status_code == 422
+
+        # Case 3: No contact information available
+        response = client.get("/billetera/contactos/minumero=4344")
+        assert response.status_code == 200
+        assert response.json() == {"contactos": []}
+
+
     # Test case for pagar endpoint
     def test_pagar(self):
         # Success case
@@ -43,6 +74,8 @@ class TestBilleteraEndpoints(unittest.TestCase):
         response = client.get("/billetera/historial/minumero=999")
         assert response.status_code == 404
         assert response.json() == {"detail": "El numero de telefono no existe"}
+
+        # Additional test cases can be added based on your specific requirements
 
 if __name__ == "__main__":
     unittest.main()
